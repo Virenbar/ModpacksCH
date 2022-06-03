f84628cb22450c054726b58bd1b55caac83ef01e
Virenbar <rib.artem@gmail.com>
2022-06-04 00:56:29 +0500

    Add interactive mode

--- ModpacksCH/Program.cs
+++ ModpacksCH/Program.cs
@@ -1,9 +1,10 @@
 ﻿using ModpacksCH.Commands;
+using Spectre.Console;
 using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Parsing;
 
-var RootCommand = new RootCommand("modpacks.ch modpack downloader") {
+var RootCommand = new RootCommand("Modpacks.ch Downloader") {
     new SearchCommand(),
     new InfoCommand(),
     new DownloadCommand()
@@ -13,7 +14,7 @@ var Parser = new CommandLineBuilder(RootCommand)
     .UseExceptionHandler()
     .UseHelp()
     .Build();
-//CBL.Invoke(args);
+
 #if DEBUG
 Parser.Invoke("s \"FTB Direwolf20\"");
 //Parser.Invoke("s gjljh;dfjh");
@@ -22,5 +23,18 @@ Parser.Invoke("s \"FTB Direwolf20\"");
 //Parser.Invoke("i 281999");
 //Parser.Invoke("d 95");
 #else
-Parser.Invoke(args);
+if (args.Length > 0) { Parser.Invoke(args); }
+else
+{
+    Parser.Invoke("-h");
+    while (true)
+    {
+        AnsiConsole.MarkupLine("[green]Input command:[/]");
+        var Input = AnsiConsole.Prompt(new TextPrompt<string>("[green]>[/]"));
+        Input = Input.Replace("ModpacksCH", "");
+        AnsiConsole.WriteLine();
+        Parser.Invoke(Input);
+        AnsiConsole.WriteLine();
+    }
+}
 #endif
